91b9dac8371f85fe928731833253ec5debcb42c7
Ondrej Platek <ondrej.platek@seznam.cz>
2013-05-21 09:47:22 -0400

    Added example of macro usage use

---
+++ test4/cffi_test4.py
@@ -0,0 +1,36 @@
+''' Testing defines and loading from source'''
+
+from cffi import FFI
+
+
+def test_verify():
+    ffi = FFI()
+    header = r'''
+    int test(void);
+    '''
+    ffi.cdef(header)
+
+    source = r'''
+    #include <stdio.h>
+    #include <math.h>
+
+    #define MY_INT 100
+
+    int test(void) {
+        printf("Printing source ugly integer makro %d!\n", MY_INT);
+        printf("Printing compile ugly integer makro %d!\n", COMPILE_INT);
+    #ifdef COMPILE_FLAG
+        printf("Compilition makro works!\n");
+    #else
+        printf("Compilition makro is off!\n");
+    #endif
+        double deg= 30.0;
+
+        return 0;
+    }
+    '''
+    lib = ffi.verify(source, define_macros=[('COMPILE_FLAG', ''), ('COMPILE_INT', '1000')])
+    lib.test()
+
+if __name__ == '__main__':
+    test_verify()
