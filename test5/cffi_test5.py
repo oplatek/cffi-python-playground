21640e8047108c68802277ce556d02a2dc5e0ca6
Ondrej Platek <ondrej.platek@seznam.cz>
2013-05-22 20:57:03 +0200

    Polished the code

--- test5/cffi_test5.py
+++ test5/cffi_test5.py
@@ -1,5 +1,8 @@
 from cffi import FFI
 
+# TODO look at passing multidimensional arrays from numpy
+# http://arjones6.blogspot.cz/2013/05/passing-multidimensional-numpy-arrays.html
+
 ffi = FFI()
 
 header = '''
@@ -7,7 +10,7 @@ typedef char mychar;
 typedef unsigned long mysize;
 int testInt(void);
 void testInt2(int * x);
-int testArray(int * x);
+void testArray(double * x, int length);
 
 void save_to_buffer(mychar ** buff, mysize * size, char * msg);
 void save_to_DoubleBuffer(double ** buff, mysize * size, char * msg);
@@ -17,30 +20,16 @@ ffi.cdef(header)
 with open('source.c', 'r') as s:
     lib = ffi.verify(s.read())
 
-buffer_pointer, buffer_size = ffi.new('double **'), ffi.new('mysize *')
-lib.save_to_DoubleBuffer(buffer_pointer, buffer_size, ffi.new('char[]', b'test 1'))
-mybytes = ffi.buffer(buffer_pointer[0], buffer_size[0])
-mybytes = mybytes[:]
 
-buffer_pointer, buffer_size = ffi.new('mychar **'), ffi.new('mysize *')
-lib.save_to_buffer(buffer_pointer, buffer_size, ffi.new('char[]', b'Ondra'))
-mybytes = ffi.string(buffer_pointer[0], buffer_size[0])
-print 'printing return char %s' % mybytes
-print 'buffer_size %d' % buffer_size[0]
-
+# returning Int by value
 print lib.testInt()
 
+# int filled by pointer
 ip = ffi.new('int *')
 ip[0] = 9
 print ip[0]
 lib.testInt2(ip)
 print ip[0]
-
-arp = ffi.new('int[20]')
-lenght = lib.testArray(arp)
-print lenght
-for i in range(lenght):
-    print arp[i]
 
 # array simple example
 ar = ffi.new("int[]", 10)
@@ -48,13 +37,31 @@ ar[3] = 3
 list_from_ar = list(ar)
 print ar, len(ar), list_from_ar
 
-
 # char simple example
 x = ffi.new("char[]", "hello")
 print x, len(x)
 print ffi.string(x)
 
+# filling double array simple
+lenght = 8
+print 'Double array lenght %d' % lenght
+arp = ffi.new('double[]', lenght)
+lib.testArray(arp, lenght)
+for i in xrange(lenght):
+    print 'arp[i]= %f' % arp[i]
+print 'Double array %s ! ' % str(arp)
 
-# cp = ffi.new('char *')
-# cp = lib.fillString(ip)
-# s = ffi.string(cp)
+# Returning array of doubles from C
+buffer_pointer, buffer_size = ffi.new('double **'), ffi.new('mysize *')
+lib.save_to_DoubleBuffer(buffer_pointer, buffer_size, ffi.new('char[]', b'junk_msg'))
+mybytes = ffi.buffer(buffer_pointer[0], buffer_size[0])
+mybytes = mybytes[:]
+print 'printing returned doubles %s as string. TODO How to convert them?' % mybytes
+print 'buffer %d' % buffer_size[0]
+
+# Returning char array (string) from C
+buffer_pointer, buffer_size = ffi.new('mychar **'), ffi.new('mysize *')
+lib.save_to_buffer(buffer_pointer, buffer_size, ffi.new('char[]', b'Ondra'))
+mybytes = ffi.string(buffer_pointer[0], buffer_size[0])
+print 'printing returned char %s' % mybytes
+print 'string %d' % buffer_size[0]
--- test5/source.c
+++ test5/source.c
@@ -4,26 +4,6 @@
 typedef char mychar;
 typedef unsigned long mysize;
 
-void save_to_DoubleBuffer(double ** buff, mysize * size, char * msg) {
-    printf("save_to_DoubleBuffer %s\n", msg);
-    *size = 30;
-    *buff = (double*) malloc(sizeof(double) * *size);
-    mysize i;
-    double pi = 3.14;
-    for (i = 0; i < *size; ++i) {
-       /* *buff[i] = pi * i;  */
-    }
-}
-
-
-void save_to_buffer(mychar ** buff, mysize * size, char * msg) {
-    printf("save_to_buffer %s\n", msg);
-    *buff = (char*) malloc(sizeof(char) * *size);
-    char *tmp = "Ahoj ";
-    *size = strlen(tmp) + strlen(msg);
-    strcpy(*buff, tmp);
-    strcat(*buff, msg);
-}
 
 int testInt(void) {
     return 3;
@@ -33,18 +13,30 @@ void testInt2(int * x) {
     *x = -6;
 }
 
-
-int testArray(int * x) {
-    /* free(x); */
-    int d = 8; 
-    /* x = (int *) malloc(d * sizeof(int)); */
-    x[3] = 33;
-    /* } */
-    return d;
+void testArray(double * x, int length) {
+    int i;
+    double pi = 3.4;
+    for (i = 0; i < length; ++i) {
+        x[i] = pi * i;
+    }
 }
 
-/* char * fillString(int * x) { */
-/*     char * tmp = "Ahoj jak se vede"; */
-/*     *x = strlen(tmp); */
-/*     return tmp; */
-/* } */
+void save_to_DoubleBuffer(double ** buff, mysize * size, char * msg) {
+    printf("save_to_DoubleBuffer %s\n", msg);
+    *size = 30;
+    *buff = (double*) malloc(sizeof(double) * *size);
+    mysize i;
+    double pi = 3.14;
+    for (i = 0; i < *size; ++i) {
+       (*buff)[i] = pi * i; 
+    }
+}
+
+void save_to_buffer(mychar ** buff, mysize * size, char * msg) {
+    printf("save_to_buffer %s\n", msg);
+    *buff = (char*) malloc(sizeof(char) * *size);
+    char *tmp = "Ahoj ";
+    *size = strlen(tmp) + strlen(msg);
+    strcpy(*buff, tmp);
+    strcat(*buff, msg);
+}
