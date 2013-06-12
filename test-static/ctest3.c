f47ca7dc9d2fa030e12de04d6924b5b52a24a461
Ondrej Platek <ondrej.platek@seznam.cz>
2013-06-12 04:44:34 -0400

    added ctest3 -from  static (ctest[1,2]) + obj(ctest3

---
+++ test-static/ctest3.c
@@ -0,0 +1,13 @@
+// -*- coding: utf-8 -*-
+// Author:   Ondrej Platek,2013, code is without any warranty!
+// Created:  11:10:29 15/04/2013
+// Modified: 11:10:29 15/04/2013
+
+// Modifed after: http://www.yolinux.com/TUTORIALS/LibraryArchives-StaticAndDynamic.html
+
+#include "ctest1.h"
+
+void ctest1(int *i)
+{
+       *i=5;
+}
